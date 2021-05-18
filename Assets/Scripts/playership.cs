96316dc069fd2dcab176333ade3442ed01d59167
Bonface Njuguna <bonface.njuguna@learninglions.org>
2021-05-18 08:39:41 +0300

    First upload of files to GitHub

---
+++ Assets/Scripts/playership.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playership : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(new Vector3(0, 0, 10) * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(new Vector3(0, 0, -10) * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(new Vector3(0, -60, 0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(new Vector3(0, 60, 0) * Time.deltaTime);
+        }
+    }
+}
