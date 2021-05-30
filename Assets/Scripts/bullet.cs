5378fcc0bb82fbc0e150ec62ff69f218ea0fc53d
Bonface Njuguna <bonface.njuguna@learninglions.org>
2021-05-30 23:47:04 +0300

    added asteroid disappear on collision code but have a weird bug, the bullet does not detect the collision precisely until very close, also the bullet is causing playership rotate when shooting

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
 
-    public float speed = 1.0f;
+    public float speed = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
